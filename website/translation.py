ffd28522031ee6cbf6cf1eb49c2ecd2429d09bbb
Aman Bothra <aman@eminenceinfotech.com>
2024-01-20 18:08:57 +0530

    initial setup project

---
+++ website/translation.py
@@ -0,0 +1,36 @@
+from modeltranslation.translator import translator, TranslationOptions
+
+from . import models
+
+
+# for Banner model
+class BannerTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+
+class AboutTranslationOptions(TranslationOptions):
+    fields = ('content', 'mobile_number', 'address')
+
+
+class CounterTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+
+class SplendorTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+
+class FeatureTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+
+class AmenitiesTranslationOptions(TranslationOptions):
+    fields = ('title', 'description')
+
+
+translator.register(models.Banner, BannerTranslationOptions)
+translator.register(models.About, AboutTranslationOptions)
+translator.register(models.Counter, CounterTranslationOptions)
+translator.register(models.Splendor, SplendorTranslationOptions)
+translator.register(models.Feature, FeatureTranslationOptions)
+translator.register(models.Amenities, AmenitiesTranslationOptions)
